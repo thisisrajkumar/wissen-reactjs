a5362046651a975521fb8acea828308b4a83d473
Raj kumar <hithisisrajkuar@gmail.com>
2021-02-06 00:02:40 +0530

    Feature:Boostrop setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import ClassCom from './comTypes/ClassCom';
 import FunCom from './comTypes/FunCom';
+import HeaderCom from './header';
 
 function App() {
   return (
     <div className="App">
+      <HeaderCom />
       <h1>React Learning</h1>
       <ClassCom />
-      <FunCom/>
+      <FunCom />
     </div>
   );
 }
